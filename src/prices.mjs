45dd7b689ecca8e67ccdea65e62cdfa563e454d7
levoskaa <47329402+levoskaa@users.noreply.github.com>
2024-02-14 22:52:40 +0100

    tcr: tests pass

--- src/prices.mjs
+++ src/prices.mjs
@@ -19,8 +19,8 @@ function createApp(database) {
     const age = req.query.age ? parseInt(req.query.age) : undefined;
     const type = req.query.type;
     const baseCost = database.findBasePriceByType(type).cost;
-    const date2 = parsePlainDate(req.query.date);
-    const cost = calculateCost(age, type, date2, baseCost);
+    const date = parsePlainDate(req.query.date);
+    const cost = calculateCost(age, type, date, baseCost);
     res.json({ cost });
   });
 
